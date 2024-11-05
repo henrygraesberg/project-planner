ade5f7bd11ca5cb562ba9d85f730d3cf39fa769c
Henry Græsberg (Mac) <henrygr@stud.ntnu.no>
2024-11-05 21:45:43 +0100

    Style auth page and navbar

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 definePageMeta({
+  layout: false,
   middleware: ['auth']
 })
 
@@ -47,23 +48,26 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="fixed top-4 w-full flex justify-center">
+    <NuxtLink to="/" class="logo font-inter font-semibold text-3xl">Outline</NuxtLink>
+  </div>
+  <div class="flex justify-center items-center h-screen">
     <form
     @submit.prevent="() => (isSignUp ? signUp() : signIn())"
-    class="flex flex-col items-center gap-4 w-[50%] p-4 bg-text-grey/60 rounded-lg"
+    class="flex flex-col items-center gap-6 min-w-52 w-[50vw] max-w-[50vw] p-4 bg-text-grey/60 rounded-lg max-h-fit"
     >
       <h2 class="font-bold text-2xl">
         {{ isSignUp ? "Create an account" : "Log into your account" }}
       </h2>
-      <input type="text" placeholder="First name" v-model="firstName" class="w-full p-2 rounded-lg" v-show="isSignUp" />
-      <input type="text" placeholder="Last name" v-model="lastName" class="w-full p-2 rounded-lg" v-show="isSignUp" />
-      <input type="email" placeholder="Email" v-model="email" class="w-full p-2 rounded-lg" />
-      <input type="password" placeholder="Password" v-model="password" class="w-full p-2 rounded-lg" />
+      <input required type="text" placeholder="First name*" v-model="firstName" class="w-full p-2 rounded-lg border-4 border-secondary-green focus:border-primary-cyan outline-none" v-if="isSignUp" />
+      <input required type="text" placeholder="Last name*" v-model="lastName" class="w-full p-2 rounded-lg border-4 border-secondary-green focus:border-primary-cyan outline-none" v-if="isSignUp" />
+      <input required type="email" placeholder="Email*" v-model="email" class="w-full p-2 rounded-lg border-4 border-secondary-green focus:border-primary-cyan outline-none" />
+      <input required type="password" placeholder="Password*" v-model="password" class="w-full p-2 rounded-lg border-4 border-secondary-green focus:border-primary-cyan outline-none" />
       <button type="submit" class="w-full bg-green-400 rounded-lg p-1">
         {{ isSignUp ? 'Sign Up' : 'Sign In' }}
       </button>
       <button type="button" @click="isSignUp = !isSignUp" class="underline">
-        {{ isSignUp ? 'Already have an account? Log in instead' : 'New user? Create an account' }}
+        {{ isSignUp ? 'Already have an account? Log in instead' : 'New to Outline? Create an account' }}
       </button>
     </form>
   </div>
